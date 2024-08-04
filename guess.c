6c8111f965c5a7b5eb990b0988a16153218eba21
kartikeyyy <kartikeykvk369@gmail.com>
2024-08-04 16:13:24 +0530

    Update guess.c

--- guess.c
+++ guess.c
@@ -2,39 +2,42 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
+int main()
+{
     // Seed the random number generator with the current time
     srand(time(0));
     // Generate a random number between 1 and 100
     int randomNumber = rand() % 100 + 1;
-    
+    int no_of_guesses = 0;
+    int guessed;
 
-    int i;
-    printf("ENTER YOUR GUESS : ");
-    scanf("%d",&i);
-
-
-
-    if(i == randomNumber)
+    do
     {
-        printf("perfect guess\n");
+        printf("ENTER YOUR GUESS : ");
+        scanf("%d", &guessed);
+
+        if (guessed < randomNumber)
+        {
+            no_of_guesses++;
+            printf("OOPS! Your guess is lower than the original number... TRY AGAIN\n");
+        }
+
+        else if (guessed > randomNumber)
+        {
+            no_of_guesses++;
+            printf("OOPS! Your guess is higher than the original number... TRY AGAIN\n");
+        }
+
+        else
+        {
+            printf("CONGRATS!!", no_of_guesses);
+        }
+
+        no_of_guesses++;
+    }
+    while (guessed != randomNumber);
+    {
+        printf("you guessed the number in %d guesses\n", no_of_guesses);
         return 0;
-
     }
-
-
-    else if(i < randomNumber)
-    {
-                printf("OOPS! Your guess is lower than the original number... TRY AGAIN\n");
-
-    }
-
-     else if(i > randomNumber)
-     {
-         printf("OOPS! Your guess is higher than the original number... TRY AGAIN\n");
-     }
-
-
-
-
 }
